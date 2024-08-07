324625f8979485abd49238df7ef8f1dd93f435a5
megait004 <giapfc123@gmail.com>
2024-08-07 15:07:23 +0700

    function

---
+++ ham/hambasic1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void display();
+void display(){
+	cout << "Xin chao 28tech" << endl;
+	cout << "Ham void" << endl;
+	cout << "28tech.com.vn";
+}
+
+int main(){
+	display();
+	return 0;
+}
