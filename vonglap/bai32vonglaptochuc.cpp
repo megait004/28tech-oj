5015432acdeaf566995a9a28eb65eb6a1d7d5519
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2024-08-13 22:50:53 +0700

    Update bai32vonglaptochuc.cpp

--- vonglap/bai32vonglaptochuc.cpp
+++ vonglap/bai32vonglaptochuc.cpp
@@ -2,18 +2,10 @@
 using namespace std;
 int main(){
 	int n ; cin >> n;
-	for(int i = 1 ; i <= n ; i++){
-		for(int j = 1; j <= n -i;j++){
-			cout << "  ";
+	int c;
+	c = 65; // vì hình này in ra chữ từ A hoa nên khai bao bien c = 65 và ép kiểu sang char
+	for(int i = 1 ; i <= n ;i++){
+		for(int j = 1 ; j <= i ;j++){
+			cout << (char)c << " ";
 		}
-		for(int j =1 ; j < i ;j++){
-			cout << "*" << " ";
-		
-		}
-		for(int j =1 ; j <= i ; j ++){
-			cout << "*"  << " " ;
-		}
-	
-		cout << endl;
-	}
-}
+		c= c +1; // sau mỗi lần in tăng biến c lên 1 đơn vị 
