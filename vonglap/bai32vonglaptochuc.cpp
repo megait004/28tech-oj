e831b7455681a806a887e6bd14b46609357ec107
megait004 <giapfc123@gmail.com>
2024-08-13 21:36:26 +0700

    feat:vonglap

---
+++ vonglap/bai32vonglaptochuc.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n ; cin >> n;
+	int c;
+	c = 65; // vì hình này in ra chữ từ A hoa nên khai bao bien c = 65 và ép kiểu sang char
+	for(int i = 1 ; i <= n ;i++){
+		for(int j = 1 ; j <= i ;j++){
+			cout << (char)c << " ";
+		}
+		c= c +1; // sau mỗi lần in tăng biến c lên 1 đơn vị 
+		cout << endl;
+	}
+}
