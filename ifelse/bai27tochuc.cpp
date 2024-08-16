50c1caf92b31cc4d2626d7f23c59afa3494d1102
megait004 <giapfc123@gmail.com>
2024-08-16 18:00:41 +0700

    feat: if else

---
+++ ifelse/bai27tochuc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main(){
+	double a ;
+	cin >> a;
+	cout << round(a); // hàm làm tròn dựa vào chữ số thập phân
+	return 0;
+}
