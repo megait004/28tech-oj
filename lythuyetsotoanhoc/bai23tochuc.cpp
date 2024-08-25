50089f780f29408b261a51950a63218b54055951
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2024-08-26 00:30:02 +0700

    Update bai23tochuc.cpp

--- lythuyetsotoanhoc/bai23tochuc.cpp
+++ lythuyetsotoanhoc/bai23tochuc.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
 	long long n ; cin >> n;
 	long long sum = 0;
-	//duy?t u?c t?i uu t? 1 d?n can c?a n
+	//duyệt ước từ 1 tới căn n 
 	for(int i = 1 ; i <= sqrt(n) ;i++){
 		if(n % i == 0){
 			sum = sum + i;
-			if(i != n / i){ // ki?m tra u?c con l?i n?u ma no khac u?c tru?c thi m?i tinh 
-			// vi d? nhu u?c c?a 16 co i la 4 u?c con l?i cung la 4 nhung ch? du?c tinh 1 l?n 
+			if(i != n / i){ //  kiểm tra ước còn lại nếu mà nó khác với ước trước thì mới tính 
+			//  ví dụ như ước của 16 là 4 thì ước còn lại cũng là 4 nên chỉ tính 1 lần
 				sum = sum + n / i;
 			}
 		}
