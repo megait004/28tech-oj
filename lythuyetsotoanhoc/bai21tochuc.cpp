1d1eb174737d1c3e818000cd16c915df9c191dc7
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2024-08-26 00:31:36 +0700

    Update bai21tochuc.cpp

--- lythuyetsotoanhoc/bai21tochuc.cpp
+++ lythuyetsotoanhoc/bai21tochuc.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 	long long a , b ;
 	cin >> a >> b ;
-	// duy?t t? i cho toi can c?a a va can c?a b , ph?i lam tron len can c?a th?ng a 
+	// ,duyệt từ i bằng căn của a cho tới căn của b , phải làm tròn căn của thằng a 
 	for(long long i = ceil(sqrt(a)) ; i <= sqrt(b); i++){
 		cout << i * i << " ";
 	}
