88071750add5aebdbfbe3de7c1489b637007e1a9
megait004 <giapfc123@gmail.com>
2024-12-27 21:20:06 +0700

    feat: email and password

---
+++ string/string18.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n ; cin >> n ;
+	cin.ignore();
+	for(int i = 0 ; i < n ; i ++){
+		string s ;
+		getline(cin, s);
+		for(char &x : s) x = tolower(x);
+		vector<string> v;
+		string w ; stringstream ss(s);
+		while(ss >> w){
+			v.push_back(w);
+		}
+		int m = v.size();
+		string email = v[m-2];
+		for(int i = 0 ; i < m - 2 ; i++){
+			email += v[i][0];
+		}
+		cout << email << "@xyz.edu.vn" << endl;
+		string ns = v[m-1];
+		stringstream ss2(ns);
+		while(getline(ss2,w,'/')){
+			cout << stoi(w);
+		}
+		cout << endl;
+	}
+}
